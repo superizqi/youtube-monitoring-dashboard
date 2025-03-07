2dde3da4a37004ab816a1e23e7e91ce8e93c9a26
Rizqi Prima <rizqiphd@gmail.com>
2025-03-07 10:20:06 +0700

    add video

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,36 @@ selected_title = st.selectbox("Select a Title", unique_titles)
 
 # Filter Data based on selected Title
 filtered_df = df[df["title"] == selected_title]
+
+with st.container():
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("❤️ Video")
+        fig1 = px.line(filtered_df, x="data_created_at", y="views_count",
+                    title=f"Views Count Over Time for {selected_title}", height=180)
+        
+        # Reduce margins, font sizes, and remove legend to fit small height
+        fig1.update_layout(
+            margin=dict(l=0, r=0, t=20, b=20),  # Reduce padding
+            font=dict(size=10),  # Smaller font
+            title=dict(font=dict(size=12)),  # Smaller title
+            xaxis=dict(title=None, tickfont=dict(size=8)),  # Smaller x-axis
+            yaxis=dict(title=None, tickfont=dict(size=8)),  # Smaller y-axis
+            legend=dict(font=dict(size=8), orientation="h")  # Horizontal legend
+        )
+        st.plotly_chart(fig1, use_container_width=True)
+
+    with col2:
+        st.subheader("📊 Views Over Time")
+        idvid = str(df[df["title"] == selected_title]['url'].iloc[0]).split("v=")[1]
+        video_url = f'https://www.youtube.com/embed/{idvid}'
+
+        st.markdown(
+            f"""
+            <iframe width="320" height="180" src="{video_url}" 
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+            </iframe>
+            """,
+            unsafe_allow_html=True
+        )
